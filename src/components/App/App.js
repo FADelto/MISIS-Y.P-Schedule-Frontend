8659a78a560e0d0487949978ce1ef5846b576e80
Alexander Chukharev <chooharev@gmail.com>
2023-11-23 01:02:37 +0300

    fix: disable button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import Navbar from '../Navbar/Navbar';
+import Button from '../Button/Button';
 import styles from './App.module.css';
 
 function App() {
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,37 +4,50 @@ import { ScheduleOutlined, UnorderedListOutlined, BellOutlined, SettingOutlined 
 import { Button, ConfigProvider } from 'antd';
 
 const Navbar = () => {
-
   const [currentPage, setCurrentPage] = useState('Расписание');
-
+  
   return (
     <ConfigProvider
     theme={{
-      token: {
-        // Seed Token
-        colorPrimary: 'rgba(0, 159, 223, 0.15)',
-        borderRadius: '15px',
-        colorText: '#009FDF',
+      components: {
+        Button: {
+          borderRadius: '15px',
+          colorPrimary: "#009FDF",
+          colorLink: '#009FDF',
+          borderColorDisabled: 'transparent',
+          colorTextDisabled: '#009FDF',
+          colorBgContainerDisabled: 'rgba(0, 159, 223, 0.15)',
+        }
       },
     }}>
       <div class={styles.navbar}>
-        <Button icon={<ScheduleOutlined />}
-              ghost={true}
-              type={currentPage === 'Расписание' ? 'primary' : 'link'}
-              onClick={() => {setCurrentPage('Расписание')}}>{currentPage === 'Расписание' ? currentPage : ''}</Button>
-        <Button icon={<UnorderedListOutlined />}
-              ghost={true}
-              type={currentPage === 'Задачи' ? 'primary' : 'link'}
-              onClick={() => {setCurrentPage('Задачи')}}>{currentPage === 'Задачи' ? currentPage : ''}
+        <Button className={styles.button}
+                icon={<ScheduleOutlined />}
+                disabled={currentPage === 'Расписание' ? true : false}
+                type={currentPage === 'Расписание' ? 'primary' : 'link'}
+                onClick={() => {setCurrentPage('Расписание')}}>
+                  {currentPage === 'Расписание' ? currentPage : ''}
         </Button>
-        <Button icon={<BellOutlined />}
-              ghost={true}
-              type={currentPage === 'Новости' ? 'primary' : 'link'} 
-              onClick={() => setCurrentPage('Новости')}>{currentPage === 'Новости' ? currentPage : ''}</Button>
-        <Button icon={<SettingOutlined />}
-              ghost={true}
-              type={currentPage === 'Настройки' ? 'primary' : 'link'} 
-              onClick={() => setCurrentPage('Настройки')}>{currentPage === 'Настройки' ? currentPage : ''}</Button>
+        <Button className={styles.button}
+                icon={<UnorderedListOutlined />}
+                disabled={currentPage === 'Задачи' ? true : false}
+                type={currentPage === 'Задачи' ? 'primary' : 'link'}
+                onClick={() => {setCurrentPage('Задачи')}}>
+                  {currentPage === 'Задачи' ? currentPage : ''}
+        </Button>
+        <Button className={styles.button}
+                icon={<BellOutlined />}
+                disabled={currentPage === 'Новости' ? true : false}
+                type={currentPage === 'Новости' ? 'primary' : 'link'} 
+                onClick={() => setCurrentPage('Новости')}>
+                  {currentPage === 'Новости' ? currentPage : ''}</Button>
+        <Button className={styles.button}
+                icon={<SettingOutlined />}
+                disabled={currentPage === 'Настройки' ? true : false}
+                type={currentPage === 'Настройки' ? 'primary' : 'link'} 
+                onClick={() => setCurrentPage('Настройки')}>
+                  {currentPage === 'Настройки' ? currentPage : ''}
+        </Button>
       </div>
     </ConfigProvider>
   )
